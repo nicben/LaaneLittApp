79f6e1b71da3d2950210a0d1ac0284e8bfc222fd
nicole bendu <nic.bendu@gmail.com>
2020-10-16 16:51:58 +0200

    logg inn, ny bruker, innstillinger ++

--- app/src/main/java/com/laanelitt/laanelittapp/profile/AddAssetFragment.kt
+++ app/src/main/java/com/laanelitt/laanelittapp/profile/AddAssetFragment.kt
@@ -20,10 +20,8 @@ class AddAssetFragment : Fragment() {
     ): View? {
        val binding = DataBindingUtil.inflate<FragmentAddAssetBinding>(inflater,R.layout.fragment_add_asset,container,false)
 
-
-
         binding.saveButton.setOnClickListener { view : View ->
-            //view.findNavController().navigate(R.id.action_addAssetFragment_to_myAssetsFragment)
+            view.findNavController().navigate(R.id.action_addAssetFragment_to_myAssetsFragment)
         }
 
         return binding.root
--- app/src/main/java/com/laanelitt/laanelittapp/profile/MyAssetsFragment.kt
+++ app/src/main/java/com/laanelitt/laanelittapp/profile/MyAssetsFragment.kt
@@ -5,11 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.addCallback
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.laanelitt.laanelittapp.Asset
 import com.laanelitt.laanelittapp.AssetListAdapter
 import com.laanelitt.laanelittapp.R
+import com.laanelitt.laanelittapp.login.LoginFragment
+import kotlinx.android.synthetic.main.fragment_my_assets.*
 
 
 //liste over alle eiendeler
@@ -42,6 +46,19 @@ class MyassetsFragment : Fragment() {
         return layout
         // Inflate the layout for this fragment
 
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // If the user presses the back button, bring them back to the home screen.
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
+            findNavController().popBackStack(R.id.searchPageFragment, false)
+        }
+
+        settings_btn.setOnClickListener {
+            findNavController().navigate(R.id.settingsFragment)
+        }
 
     }
 
